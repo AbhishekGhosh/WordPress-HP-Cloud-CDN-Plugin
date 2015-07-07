39777f2b3069a49efafaa292df7adea836db598f
Abhishek Ghosh <dr.abhishek_ghosh@hotmail.com>
2015-07-07 23:07:23 +0530

    Create shell.php

---
+++ shell.php
@@ -0,0 +1,7 @@
+<?php
+// nothing
+//defined('WP_PLUGIN_URL') or die('Restricted access');
+?>
+<p>Note that, this is an advanced plugin and by default the function to work is deliberately deactivated.</p>
+<p>SSH access with sudo privilege is required to activate this Plugin.</p>
+<p>This Plugin is designed for HP Helion OpenStack to upload Objects from nodes to OpenStack Object Storage ( previously known as OpenStack SWIFT ).</p>
